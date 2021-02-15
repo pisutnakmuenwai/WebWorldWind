0518fd59c846d69e479e863960ce593a24231c5a
Pisut Nakmuenwai <pisut@gistda.or.th>
2021-02-15 14:17:34 +0700

    fixed CompassLayer pass argument to Compass

--- src/layer/CompassLayer.js
+++ src/layer/CompassLayer.js
@@ -44,10 +44,10 @@ define([
          * @classdesc Displays a compass. Compass layers cannot be shared among WorldWindows. Each WorldWindow if it
          * is to have a compass layer must have its own. See the MultiWindow example for guidance.
          */
-        var CompassLayer = function () {
+        var CompassLayer = function (screenOffset = null, imagePath = null) {
             RenderableLayer.call(this, "Compass");
 
-            this._compass = new Compass(null, null);
+            this._compass = new Compass(screenOffset, imagePath);
 
             this.addRenderable(this._compass);
         };
